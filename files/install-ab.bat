a9b035541aea5dd2632d4b9941390c4d511d72f4
ceres100 <115903934+aloceres@users.noreply.github.com>
2024-05-18 00:23:40 -0400

    Add files via upload

---
+++ files/install-ab.bat
@@ -0,0 +1,22 @@
+@echo off
+
+echo Installing GSI on AB device
+
+fastboot reboot fastboot
+echo Erasing system
+fastboot erase system
+TIMEOUT /T 3
+
+set /p slot="Enter slot letter: "
+echo Deleting product_%slot% partiton
+fastboot delete-logical-partition product_%slot%
+TIMEOUT /T 3
+
+echo Flashing system. Please be patient...
+fastboot flash system system.img
+
+echo Finished. Format data from recovery and reboot.
+fastboot reboot recovery
+
+echo Process ended
+TIMEOUT /T 30
---
+++ files/install-aonly.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo Installing GSI on A only device
+
+fastboot reboot fastboot
+echo Erasing system
+fastboot erase system
+TIMEOUT /T 3
+
+echo Deleting product partiton
+fastboot delete-logical-partition product
+TIMEOUT /T 3
+
+echo Flashing system. Please be patient...
+fastboot flash system system.img
+
+echo Finished. Format data from recovery and reboot.
+fastboot reboot recovery
+
+echo Process ended
+TIMEOUT /T 30
